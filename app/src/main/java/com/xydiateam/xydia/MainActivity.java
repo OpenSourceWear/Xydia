81e872e4b876030d51f64e17f67d8b3637d6b47c
GrieferPig <grieferpig@163.com>
2020-10-21 16:01:15 +0800

    Added App object

--- app/src/main/java/com/xydiateam/xydia/MainActivity.java
+++ app/src/main/java/com/xydiateam/xydia/MainActivity.java
@@ -1,7 +1,6 @@
 package com.xydiateam.xydia;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        App app = new App("a", "simple", "test", 666);
     }
 }
---
+++ app/src/main/java/com/xydiateam/xydia/appGenre.java
@@ -0,0 +1,10 @@
+package com.xydiateam.xydia;
+
+public class appGenre {
+    public int NONE = 0;
+    public int UTILITIES = 1;
+    public int ENTERTAINMENT = 2;
+    public int MULTIMEDIA = 3;
+    public int SYSTEM_MODIFIER = 4;
+    public int GAME = 5;
+}
